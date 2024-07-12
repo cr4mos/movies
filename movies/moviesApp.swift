b089787f7762b933678c7177dbf5783f28c0450d
Carlos Ramos <carlos.930919@gmail.com>
2024-07-12 14:15:15 -0600

    Initial Commit

---
+++ movies/moviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  moviesApp.swift
+//  movies
+//
+//  Created by Carlos Ramos on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct moviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
